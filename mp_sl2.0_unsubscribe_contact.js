9d0f8023c9cb44ddc1758a1de1de3ab5e2578312
ankith88 <ankith88@gmail.com>
2024-06-28 11:52:05 +1000

    CU-86cvu6key

---
+++ mp_sl2.0_unsubscribe_contact.js
@@ -0,0 +1,77 @@
+/**
+ * @NApiVersion 2.0
+ * @NScriptType Suitelet
+ * Author:               Ankith Ravindran
+ * Created on:           Fri Jun 28 2024
+ * Modified on:          Fri Jun 28 2024 11:22:54
+ * SuiteScript Version:  2.0 
+ * Description:          Ability to unsubscribe a contact from receiving future EDM emails 
+ *
+ * Copyright (c) 2024 MailPlus Pty. Ltd.
+ */
+
+
+
+define(['N/ui/serverWidget', 'N/email', 'N/runtime', 'N/search', 'N/record', 'N/https', 'N/log', 'N/redirect', 'N/url', 'N/format', 'N/task'],
+    function (ui, email, runtime, search, record, https, log, redirect, url, format, task) {
+        var role = 0;
+        var userId = 0;
+        var zee = 0;
+
+        function onRequest(context) {
+            var baseURL = 'https://system.na2.netsuite.com';
+            if (runtime.EnvType == "SANDBOX") {
+                baseURL = 'https://system.sandbox.netsuite.com';
+            }
+
+            if (context.request.method === 'GET') {
+
+                var custinternalid = context.request.parameters.custinternalid;
+                var contactid = context.request.parameters.contactid;
+
+
+                var form = ui.createForm({
+                    title: 'You have successfully opted out.'
+                });
+
+                var contactRecord = record.load({
+                    type: record.Type.CONTACT,
+                    id: parseInt(contactid),
+                    isDynamic: true
+                });
+
+                contactRecord.setValue({
+                    fieldId: 'custentity_create_password_email',
+                    value: 1
+                });
+
+                contactRecord.save();
+
+
+                form.addField({
+                    id: 'preview_table',
+                    label: 'inlinehtml',
+                    type: 'inlinehtml'
+                }).updateLayoutType({
+                    layoutType: ui.FieldLayoutType.STARTROW
+                }).defaultValue = inlineHtml;
+
+
+                context.response.writePage(form);
+            } else {
+
+            }
+        }
+
+
+        function isNullorEmpty(val) {
+            if (val == '' || val == null) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return {
+            onRequest: onRequest
+        };
+    });
